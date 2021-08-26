f465d627ac04203a045addd245a773a20212b955
AwesomeNicknameEver <katerina.muskova@gmail.com>
2021-08-26 16:43:03 +0200

    plots ok

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import geopandas as gpd
 import numpy as np
 import seaborn as sns
-
+from sklearn.cluster import KMeans
 from matplotlib import pyplot as plt
 
 
@@ -51,22 +51,52 @@ def get_geo_map():
     return geo_map
 
 
+def add_kmeans_classes(df):
+    km = df.sort_values('increase_perc', ).copy()
+
+    km = km.loc[~df['increase_perc'].isna()]
+    km = km.loc[:, ['increase_perc', 'country']]
+    km['2d'] = km['increase_perc']
+
+    kmeans = KMeans(n_clusters=3).fit(km[['increase_perc', '2d']])
+    km['class'] = kmeans.labels_
+    km = km.loc[:, ['class', 'country']]
+
+    df = df.join(km.set_index('country'), on='country', how='left')
+
+    df.loc[df['increase_perc'] < 0, 'class'] = 3
+
+    df = df.astype({'class': 'category'})
+
+    level_mean = df.loc[:, ['class', 'increase_perc']].groupby('class').mean().reset_index()
+    level_mean = level_mean.rename({'increase_perc': 'mean_cls'}, axis=1)
+
+    df = df.join(level_mean.set_index('class'), on='class', how='left')
+    df['class'] = df['mean_cls'].round(decimals=1)
+    df = df.drop('mean_cls', axis=1)
+
+    df = df.astype({'class': 'category'})
+    return df
+
+
 def plot_geo(df: pd.DataFrame, fig_location: str = None, show_figure: bool = False):
     geo_map = get_geo_map()
 
     df = df.loc[~ df['country'].isin(['EA18', 'EA19', 'EU27_2007', 'EU27_2020', 'EU28', 'FX', 'XK'])]
     df = geo_map.join(df.set_index('country'), on='country', how='right')
 
+    df = add_kmeans_classes(df)
+
     fig, axes = plt.subplots(1, 2, figsize=(11.69, 5))
     fig.suptitle("Motality increase in Europe 2020", fontsize=16)
 
     axes = axes.flatten()
-    plot_columns = ['increase', 'increase_perc']
+    plot_columns = ['increase', 'class']
 
     for i in range(2):
         df.plot(column=plot_columns[i], ax=axes[i], missing_kwds=dict(color="lightgrey", ), legend=True, cmap='hot')
 
-    ax_titles = ['Total increase per 1000 inhabitants', 'Percentage increase per 1000 inhabitants']
+    ax_titles = ['Total increase per 1000 inhabitants', 'Average of percentage increase']
 
     for i in range(2):
         axes[i].xaxis.set_visible(False)
@@ -94,7 +124,7 @@ def add_country_names(df: pd.DataFrame):
 
 def plot_table(df: pd.DataFrame, fig_location: str = None, show_figure: bool = False):
     df = add_country_names(df)
-    df.sort_values('increase', inplace=True)
+    df.sort_values('increase_perc', inplace=True)
     df = df.loc[~df['2020'].isna()]
     df.loc[df['country'] == 'MK', 'NAME'] = 'Macedonia'
 
@@ -129,9 +159,35 @@ def save_table(df: pd.DataFrame, table_location: str = None):
         df.to_csv(table_location)
 
 
+def print_mean_europe(df):
+    print('AVG EU:')
+    print(df.loc[df['country'] == 'EU27_2020', ['mean', '2020']])
+
+
+def print_sweden_increase(df):
+    print('Sweden 2020:')
+    print(df.loc[df['country'] == 'SE', '2020'])
+
+
+def print_poland_increase(df):
+    print('Poland perc increase:')
+    print(df.loc[df['country'] == 'PL', ['increase_perc', '2020']])
+
+
+def print_czech_increase(df):
+    print('CR :')
+    print(df.loc[df['country'] == 'CZ', ['increase_perc', '2020']])
+
+
 def main():
     df = get_mortality()
-    plot_table(df, show_figure=True, fig_location='01_barplot.png')
+
+    print_mean_europe(df)
+    print_sweden_increase(df)
+    print_poland_increase(df)
+    print_czech_increase(df)
+
+    plot_table(df, show_figure=False, fig_location='01_barplot.png')
     plot_geo(df, show_figure=False, fig_location='02_map.png')
 
     save_table(df, 'mortality_table.csv')
